5c518210d8ddb35dfeae54af0ae44ab2634fa2b0
shreempy <shreempy@gmail.com>
2022-05-26 17:22:04 +0200

    subnet/vnet fix

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,17 +1,8 @@
-data "azurerm_virtual_network" "myvnet" {
-  name = var.vnet_name
-  resource_group_name = var.rg_name
-
-}
-
-
-
-
 resource "azurerm_virtual_network" "myvnet" {
-  name = data.azurerm_virtual_network.myvnet.name
+  name = var.vnet_name
   location            = var.deploy_location
   resource_group_name = var.rg_name
-    address_space = data.azurerm_virtual_network.myvnet.address_space
+    address_space = ["10.0.0.0/16"]
     subnet {
     name           = var.subnet_name
     address_prefix = "10.0.1.0/24"
